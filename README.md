b96232bf8d58e4b3fafe1cce88308d4eeb9fe008
Larry Liang <larryleang@Larrys-MBP.home>
2020-12-13 14:47:09 -0500

    initial release

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Portfolio
+[![Gatsby](https://img.shields.io/badge/Gatsby-2.28.2-blueviolet)](https://www.gatsbyjs.com)
+[![React](https://img.shields.io/badge/create--react--app-4.0.1-blue)](https://reactjs.org)
+[![Node.js](https://img.shields.io/badge/node.js--LTS-14.15.1-green)](https://nodejs.org/dist/v14.15.1/)
+![LICENSE](https://shields.io/badge/license-MIT-green)
+
+[**Website**](https://larrylx.com)
---
+++ gatsby-config.js
@@ -0,0 +1,78 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // Of course you can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: `larrylx`,
+    siteTitleAlt: `Larry Liang Portfolio`,
+    siteHeadline: `larrylx - Gatsby Theme from @lekoarts`,
+    siteUrl: `https://larrylx.com`,
+    siteDescription: `Portfolio`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@larrylx`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `sections`,
+        path: `${__dirname}/src/sections`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `larrylx - @larrylx/gatsby-theme-lx`,
+        short_name: `larrylx`,
+        description: `Description Here`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {},
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
